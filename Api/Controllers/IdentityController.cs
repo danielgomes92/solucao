a5a65ea05e714cf57800f4bddea1485cc8df42a1
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-08-26 16:37:52 -0300

    localhost adicionado e acessível
    projeto configurando API funcionando.

--- Api/Controllers/IdentityController.cs
+++ Api/Controllers/IdentityController.cs
@@ -1,6 +1,21 @@
-﻿namespace Api.Controllers
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
 {
-    public class IdentityController
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult
+                (
+                    from c in User.Claims
+                    select new { c.Type, c.Value }
+                );
+        }
     }
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api
 {
@@ -26,6 +27,18 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddAuthentication("Bearer") // nome do esquema ao portador que estamos utilizando.
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                    options.TokenValidationParameters
+                    = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +53,8 @@ namespace Api
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication(); //acionada para que a autenticão seja executada automaticamente
+            app.UseAuthorization();  // garante que não possa ser acessado por anonimos
 
             app.UseEndpoints(endpoints =>
             {
